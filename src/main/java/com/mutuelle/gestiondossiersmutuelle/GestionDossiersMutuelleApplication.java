4db23304d838278e632c3a86afc25b85dba424ff
tanafaat <tanafaatabdessamad@gmail.com>
2024-11-12 13:37:27 +0100

    configuration

---
+++ src/main/java/com/mutuelle/gestiondossiersmutuelle/GestionDossiersMutuelleApplication.java
@@ -0,0 +1,13 @@
+package com.mutuelle.gestiondossiersmutuelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDossiersMutuelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDossiersMutuelleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mutuelle/gestiondossiersmutuelle/GestionDossiersMutuelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutuelle.gestiondossiersmutuelle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDossiersMutuelleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
